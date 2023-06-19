1e0cfd818f3800ef5c1b80b08014ddd393128a86
adamd <adam.drozdz1987@gmail.com>
2023-06-19 23:44:30 +0200

    tt stream

---
+++ twitter-to-kafka-service/src/main/java/pl/adamd/microservice/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,41 @@
+package pl.adamd.microservice.twitter.to.kafka.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.adamd.microservice.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import pl.adamd.microservice.twitter.to.kafka.service.runner.StreamRunner;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    private final StreamRunner streamRunner;
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData,
+                                            StreamRunner streamRunner) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args)
+            throws Exception {
+       LOG.info("App starts..");
+       LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+       LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+       streamRunner.start();
+
+    }
+}
---
+++ twitter-to-kafka-service/src/main/java/pl/adamd/microservice/twitter/to/kafka/service/runner/StreamRunner.java
@@ -0,0 +1,7 @@
+package pl.adamd.microservice.twitter.to.kafka.service.runner;
+
+import twitter4j.TwitterException;
+
+public interface StreamRunner {
+    void start() throws TwitterException;
+}
